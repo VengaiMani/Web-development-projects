82b2936527f600b9540c089b927e7bdfa343092a
VengaiMani <vengaimani17@gmail.com>
2020-12-26 12:04:02 +0530

    Adding variables

--- Stopwatch/scripts.js
+++ Stopwatch/scripts.js
@@ -1,3 +1,8 @@
 window.onload=function(){
-    alert("Hi there")
+    var start=document.getElementById("start");
+    var stop=document.getElementById("stop");
+    var reset=document.getElementById("reset");
+    var seconds=document.getElementById("seconds");
+    var tens=document.getElementById("tens");
+
 }
